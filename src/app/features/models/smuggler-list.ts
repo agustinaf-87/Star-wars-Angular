cca109ed3d47acbd0c8693bf1a1aabc156dcb897
AgustinaElenaFernand <fernandez.agustinaelena@gmail.com>
2023-04-12 10:49:31 +0200

    Added data and models closes #5

---
+++ src/app/features/models/smuggler-list.ts
@@ -0,0 +1,66 @@
+import { ISmuggler } from "../interfaces/ISmuggler";
+
+
+export const SmuggerList: ISmuggler[] = [
+  {
+    id: 1,
+    name: "Han Solo",
+    image: "src/assets/img/img-characters/hansolo.jpg",
+    birthPlanet: "Corellia",
+    birthDay: "29 ABY",
+    imageSpacecraft: "src/assets/img/img-ship/halconM.png",
+    shipName: "Halcón Milenario",
+    model: "Carguero ligero YT-1300",
+    manufacturer: "Corporación de Ingeniería Corelliana",
+    imagePlanet: "src/assets/img/img-planets/Corellia.jpg",
+    namePlanet: "Corellia",
+    moons: 3,
+    suns: 1
+  },
+  {
+    id: 2,
+    name: "Chewbacca",
+    image: "src/assets/img/img-characters/Chewie.jpg",
+    birthPlanet: "Kashyyyk",
+    birthDay: "200 ABY",
+    imageSpacecraft: "src/assets/img/img-ship/halconM.png",
+    shipName: "Halcón Milenario",
+    model: "Carguero ligero YT-1300",
+    manufacturer: "Corporación de Ingeniería Corelliana",
+    imagePlanet: "src/assets/img/img-planets/Kashyyyk.jpg",
+    namePlanet: "Kashyyyk",
+    moons: 3,
+    suns: 1
+  },
+  {
+    id: 3,
+    name: "Mara Jade Skywalker",
+    image: "src/assets/img/img-characters/mara.jpg",
+    birthPlanet: "Coruscant",
+    birthDay: "17 ABY",
+    imageSpacecraft: "src/assets/img/img-ship/Jade_Shadow.jpg",
+    shipName: "Sombra de Jade",
+    model: "Yate Estelar clase Horizon",
+    manufacturer: "Corporación SoroSuub",
+    imagePlanet: "src/assets/img/img-planets/Coruscant.jpg",
+    namePlanet: "Coruscant",
+    moons: 4,
+    suns: 1
+
+  },
+  {
+    id: 4,
+    name: "Lando Calrissian",
+    image: "src/assets/img/img-characters/Lando.jpg",
+    birthPlanet: "Socorro",
+    birthDay: "31 ABY",
+    imageSpacecraft: "src/assets/img/img-ship/halconM.png",
+    shipName: "Halcón Milenario",
+    model: "Carguero ligero YT-1300",
+    manufacturer: "Corporación de Ingeniería Corelliana",
+    imagePlanet: "src/assets/img/img-planets/socorro.png",
+    namePlanet: "Socorro",
+    moons: 0,
+    suns: 1
+  },
+];
