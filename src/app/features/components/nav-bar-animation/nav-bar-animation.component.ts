416867589a970552e51765c54461e0d8431d0a77
AgustinaElenaFernand <fernandez.agustinaelena@gmail.com>
2023-04-12 13:44:34 +0200

    Added general changes on homepage, cards, animations closes #24

---
+++ src/app/features/components/nav-bar-animation/nav-bar-animation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-nav-bar-animation",
+  templateUrl: "./nav-bar-animation.component.html",
+  styleUrls: ["./nav-bar-animation.component.scss"],
+})
+export class NavBarAnimationComponent {}
